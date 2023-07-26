f6b2b62ae50525179094b8590791d8efdb4f157c
Girijesh Thodupunuri <girinuri123@gmail.com>
2023-07-26 15:35:50 -0700

    created register page

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import CreateUser from './CreateUser.jsx';
+
+function App() {
+
+  return (
+   <div>
+      <CreateUser/>
+   </div>
+  )
+}
+
+export default App

e6aa12eba398aa75d73bc25fcffad21c545afab3
Girijesh Thodupunuri <girinuri123@gmail.com>
2023-07-26 20:21:35 -0700

    wrapped app around context and created new comp for logged in

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,25 @@
-import { useState } from 'react';
-import axios from 'axios';
-import CreateUser from './CreateUser.jsx';
-import {LoggedInUserContextProvider} from './Contexts/LoggedInUserContxt.jsx';
+import { useState, useContext } from "react";
+import axios from "axios";
+import CreateUser from "./CreateUser.jsx";
+import {LoggedInUserContext} from './Contexts/LoggedInUserContxt';
 
 function App() {
-  axios.defaults.baseURL="http://localhost:3001";
+  axios.defaults.baseURL = "http://localhost:3001";
   axios.defaults.withCredentials = true;
+  const {loggedInUser} = useContext(LoggedInUserContext);
+  console.log(loggedInUser);
+  if(!loggedInUser){
+    return (
+      <div>
+        <CreateUser />
+      </div>
+    );
+  }
   return (
-   <div>
-    <LoggedInUserContextProvider>
-      <CreateUser/>
-    </LoggedInUserContextProvider>
-   </div>
+    <>
+    <h1>User Logged in </h1>
+    </>
   )
 }
 
-export default App
+export default App;
